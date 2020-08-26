87f90cd5ddbce02f4455ca682e935dd80ae283c3
Jochem Brouwer <jochembrouwer96@gmail.com>
2020-08-26 23:20:54 +0200

    [VM] block now transitions to new HF on a transition block

--- packages/vm/lib/index.ts
+++ packages/vm/lib/index.ts
@@ -145,6 +145,10 @@ export default class VM extends AsyncEventEmitter {
     this._emit = promisify(this.emit.bind(this))
   }
 
+  _updateOpcodes() {
+    this._opcodes = getOpcodesForHF(this._common)
+  }
+
   async init(): Promise<void> {
     if (this.isInitialized) {
       return
